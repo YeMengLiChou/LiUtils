e6121cc677f07e689915c4f49c11a0956d8760e7
YeMengLiChou <swk2460050897@outlook.com>
2023-09-26 15:56:32 +0800

    init utils packages

---
+++ LiFramework/src/main/java/com/li/framework/base/interfaces/IViewModel.kt
@@ -0,0 +1,32 @@
+package com.li.framework.base.interfaces
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlin.reflect.KClass
+
+/**
+ *
+ * 用于初始化 [ViewModel] 的接口
+ *
+ * @author Gleamrise
+ * <br/>Created: 2023/09/26
+ */
+interface IViewModel<VM: ViewModel> {
+
+    /**
+     * 指定与 Activity/Fragment 相关联的 ViewModel
+     * */
+    fun getViewModelClass(): Class<VM>
+
+    /**
+     * 创建 ViewModel 的 Factory，如果有依赖项，则自定义 Factory
+     * */
+    fun getViewModelFactory(): ViewModelProvider.Factory
+
+
+    fun getCreationExtras(): CreationExtras
+
+    fun initViewModel(): VM
+
+}
